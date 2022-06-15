7d664f0836f8866aef07a8e3ef42591ab1bb5328
Ale-Costa <alexandrercosta01@gmail.com>
2022-06-15 17:32:54 -0300

    callback

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 "use strict";
-function somarValores(input1, input2) {
-    if (typeof input1 === "string" || typeof input2 === "string") {
-        return input1.toString() + input2.toString();
-    }
-    else {
-        return input1 + input2;
-    }
+function printaValoresNumericos(numero1, numero2) {
+    console.log(numero1 + numero2);
 }
-console.log(somarValores(5, 11));
-console.log(somarValores('where', ' is the car?'));
-console.log(somarValores('where', 10));
+function somaValoresNumericos(numero1, numero2, callback) {
+    let resultado = numero1 + numero2;
+    return callback(resultado);
+}
+function aoQuadrado(numero) {
+    return numero * numero;
+}
+function dividirPorEleMesmo(numero) {
+    return numero / numero;
+}
+console.log(somaValoresNumericos(1, 3, aoQuadrado));
+console.log(somaValoresNumericos(1, 3, dividirPorEleMesmo));
--- app.ts
+++ app.ts
@@ -1,14 +1,20 @@
-
-type input = number | string;
-
-function somarValores(input1: input, input2: input){
-    if (typeof input1 === "string" || typeof input2 === "string"){
-        return input1.toString() + input2.toString();
-    }else{
-        return input1 + input2;
-    }
+function printaValoresNumericos(numero1: number, numero2: number): void{
+    console.log(numero1 + numero2);
 }
 
-console.log(somarValores(5,11));
-console.log(somarValores('where',' is the car?'));
-console.log(somarValores('where',10));
+
+function somaValoresNumericos(numero1: number, numero2: number, callback: (numero:number)=> number): number{
+    let resultado = numero1 + numero2;
+    return callback(resultado);
+}
+
+function aoQuadrado(numero: number):number{
+    return numero * numero;
+}
+
+function dividirPorEleMesmo(numero: number): number{
+    return numero / numero
+}
+
+console.log(somaValoresNumericos(1,3, aoQuadrado));
+console.log(somaValoresNumericos(1,3, dividirPorEleMesmo));
